2128205fba02a581c42c84f268c533cade409c71
Lizzie <74326237+cutiepie44@users.noreply.github.com>
2021-04-26 13:50:48 -0400

    Update suace.py
    New Look.

--- suace.py
+++ suace.py
@@ -1,13 +1,24 @@
 import time
 import webbrowser
-print("suace+")
+print("""                                                              
+ @@@@@@   @@@  @@@   @@@@@@    @@@@@@@  @@@@@@@@             
+@@@@@@@   @@@  @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@             
+!@@       @@!  @@@  @@!  @@@  !@@       @@!          @@!     
+!@!       !@!  @!@  !@!  @!@  !@!       !@!          !@!     
+!!@@!!    @!@  !@!  @!@!@!@!  !@!       @!!!:!    @!@!@!@!@  
+ !!@!!!   !@!  !!!  !!!@!!!!  !!!       !!!!!:    !!!@!@!!!  
+     !:!  !!:  !!!  !!:  !!!  :!!       !!:          !!:     
+    !:!   :!:  !:!  :!:  !:!  :!:       :!:          :!:     
+:::: ::   ::::: ::  ::   :::   ::: :::   :: ::::             
+:: : :     : :  :    :   : :   :: :: :  : :: ::              
+                                                             """)
 x = 1
 while True:
     code = input("whats the code ")
     website = "https://nhentai.net/g/"
     print(website + code)
     webbrowser.open(f"{website}{code}")
-    time.sleep (5)
     print('\033c')
     print('\x1bc')
+    time.sleep (5)
 time.sleep (5)
