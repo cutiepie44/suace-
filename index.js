78dcb1e33ebdd3938fa4adc3e6fddc314b8db3ee
ballsballsballsballsballsballsballsball <sorenzio69@gmail.com>
2021-04-26 17:25:41 -0500

    ohyes

---
+++ index.js
@@ -0,0 +1,13 @@
+const opn = require('open');
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("whats da code", function(e) {
+    const url = `https://nhentai.net/g/${e}`;
+    opn(url);
+    console.log(url);
+    rl.close();
+});
