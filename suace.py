38f099f37ddcbda380861d4317f54688ca6c02bf
Lizzie <74326237+cutiepie44@users.noreply.github.com>
2021-04-26 12:55:04 -0400

    Update suace.py
    The Program Does not end after one source..

--- suace.py
+++ suace.py
@@ -1,8 +1,13 @@
 import time
 import webbrowser
 print("suace+")
-code = input("whats the code ")
-website = "https://nhentai.net/g/"
-print(website + code)   
-webbrowser.open(f"{website}{code}")
+x = 1
+while True:
+    code = input("whats the code ")
+    website = "https://nhentai.net/g/"
+    print(website + code)
+    webbrowser.open(f"{website}{code}")
+    time.sleep (5)
+    print('\033c')
+    print('\x1bc')
 time.sleep (5)
