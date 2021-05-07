34168306a02736f09ff983eb5859aa1125bbb441
Lizzie <74326237+cutiepie44@users.noreply.github.com>
2021-05-07 10:17:51 -0400

    NEW UPDATE!!
    NOW WORKS WITH R34 AND E621.

--- suace.py
+++ suace.py
@@ -12,13 +12,34 @@ print("""
 :::: ::   ::::: ::  ::   :::   ::: :::   :: ::::             
 :: : :     : :  :    :   : :   :: :: :  : :: ::              
                                                              """)
-x = 1
-while True:
-    code = input("whats the code ")
-    website = "https://nhentai.net/g/"
-    print(website + code)
-    webbrowser.open(f"{website}{code}")
-    print('\033c')
-    print('\x1bc')
-    time.sleep (5)
+websitec = input("What website? ")
+if websitec == "1":
+    websitec = 1
+    while True:
+        code0 = input("whats the code ")
+        website0 = "https://nhentai.net/g/"
+        print(website0 + code)
+        webbrowser.open(f"{website0}{code0}")
+        print('\033c')
+        print('\x1bc')
+        time.sleep (5)
+if websitec == "2":
+                websitec = 2
+                while True:
+                    code1 = input("whats the code ")
+                    website1 = "https://rule34.xxx/index.php?page=post&s=view&id="
+                    print(website1 + code1)
+                    webbrowser.open(f"{website1}{code1}")
+                    print('\033c')
+                    print('\x1bc')
+                    time.sleep (5)
+if websitec == "3":
+    websitec = 3
+    while True:
+        code2 = input("whats the code ")
+        website2 = "https://e621.net/posts/"
+        print(website2 + code2)
+        webbrowser.open(f"{website2}{code2}")
+        print('\033c')
+        print('\x1bc')
 time.sleep (5)
